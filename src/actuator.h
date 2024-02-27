d39ae9ec20af741f2f872b33cbad1e5d2c5fcc4f
mohammad.askari <mohammad.askari@epfl.ch>
2024-02-27 23:37:53 +0300

    update actuator class

--- src/actuator.h
+++ src/actuator.h
@@ -18,9 +18,9 @@ typedef enum
 class Actuator
 {
 public:
-  int8_t position;
   int8_t offset;
   int8_t range;
+  int8_t position;
   float frequency;
   drive_t mode;
   Servo servo;
@@ -28,8 +28,8 @@ public:
   // object constructor
   Actuator() {}
 
-  // sets initial variables, and attaches servo pins
-  void init(uint8_t pin, int8_t offset, int8_t range, 
+  // initializes variables, attaches servo pins, and moves to initial position
+  void init(uint8_t pin, int8_t offset, int8_t range,
             float frequency = 0, drive_t mode = drive_t::STEP)
   {
     this->pin = pin;
@@ -41,15 +41,27 @@ public:
     this->servo.attach(pin);
 
     servo.attach(pin);
-    servo.write(position);
+    setConstraints();
+    move();
   }
 
+  // set variable methods
+  void setRange(int8_t range) { this->range = range; setConstraints(); }
+  void setOffset(int8_t offset) { this->offset = offset; setConstraints(); }
+  void setPosition(int8_t position) { this->position = position; move(); }
+
+  void setConstraints()
+  {
+    int8_t rng = abs(range);
+    min_pos = constrain(offset - rng, RANGE_MIN, RANGE_MAX);
+    max_pos = constrain(offset + rng, RANGE_MIN, RANGE_MAX);
+  }
+
+  // main methods to move the servo, or reset its position back to the neutral state
   void move()
   {
-    int min_pos = offset - range;
-    int max_pos = offset + range;
-    int pos     = constrain(position, min_pos, max_pos);
-    int angle   = map(pos, RANGE_MIN, RANGE_MAX, SERVO_MIN, SERVO_MAX);
+    int pos = constrain(position, min_pos, max_pos);
+    int angle = map(pos, RANGE_MIN, RANGE_MAX, SERVO_MIN, SERVO_MAX);
     servo.write(angle);
   }
 
@@ -59,16 +71,20 @@ public:
     move();
   }
 
+  // print variables for debugging purposes
   void print()
   {
-    char str[80];
-    sprintf(str, "pin: %d\tpos: %d\toffset: %4d\t range: %d\t freq: %.1f\t mode: %d",
-            pin, position, offset, range, frequency, mode);
+    byte len = 80;
+    char str[len];
+    snprintf(str, len, "pin: %4d, pos: %4d, offset: %4d, range: %4d, freq: %.1f"
+             ", mode: %d", pin, position, offset, range, frequency, mode);
     Serial.println(str);
   }
 
 private:
   uint8_t pin;
+  int8_t min_pos;
+  int8_t max_pos;
 };
 
 #endif
